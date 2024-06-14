d24ee663df11116118f3374bffb501e26e87f0ba
Ryosei Teraji <c0a2416112@edu.teu.ac.jp>
2024-06-14 18:28:01 +0900

    kadaiii

--- live_share.py
+++ live_share.py
@@ -3,3 +3,5 @@ gakuseki_num2 = "C0A24161" #C0A24161
 num1 = int(gakuseki_num1[5] + gakuseki_num1[6] + gakuseki_num1[7]) #C0A24179
 num2 = int(gakuseki_num2[5] + gakuseki_num2[6] + gakuseki_num2[7]) #C0A24161 
 print("変数1と2を足すと" + str(num1 + num2) + "になります。") #C0A24179,C0A24161
+
+print("変数1と変数2をかけると" + str(num1*num2))
