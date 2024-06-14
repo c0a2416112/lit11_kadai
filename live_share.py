594fc03af9245486fd5c64c3a73cc35f97ad2a7f
Ryosei Teraji <c0a2416112@edu.teu.ac.jp>
2024-06-14 18:14:05 +0900

    first commit

---
+++ live_share.py
@@ -0,0 +1,5 @@
+gakuseki_num1 = "C0A24179" #C0A24179
+gakuseki_num2 = "C0A24161" #C0A24161
+num1 = int(gakuseki_num1[5] + gakuseki_num1[6] + gakuseki_num1[7]) #C0A24179
+num2 = int(gakuseki_num2[5] + gakuseki_num2[6] + gakuseki_num2[7]) #C0A24161 
+print("変数1と2を足すと" + str(num1 + num2) + "になります。") #C0A24179,C0A24161
